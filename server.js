14b55762470fac53a7016ec0069e08da91044c9d
HasanOmar1 <r3domar1@gmail.com>
2023-12-30 14:28:13 +0200

    added

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ import cors from "cors";
 import "dotenv/config";
 import { errorHandler } from "./middlewares/errorMiddleware.js";
 import bankRoutes from "./routes/bankRoutes.js";
+import fs from "fs";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  const indexHtml = fs.readFileSync("./constants/index.html", "utf-8");
+  res.send(indexHtml);
+});
 app.use("/api/v1/bank", bankRoutes);
 
 app.use(errorHandler);
